0ddd5ce1f8969b99d5ffefce72f248942e7211e1
Lucas Carrias <lucascarrias@outlook.com>
2020-08-04 11:01:34 -0300

    Criar README.md

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get("/users", (req, res) => {
-    res.json({"user":"Lucas"});
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.json({message:"Hello World."});
 });
 
 app.listen(3333);
